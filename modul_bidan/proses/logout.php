4527dc04ab836a009cb8fbaf947a521e3e44648b
nadyakhairunnisa <nadya.khairunnisa04@gmail.com>
2017-10-31 08:53:53 +0700

    bidan (2)

---
+++ modul_bidan/proses/logout.php
@@ -0,0 +1,10 @@
+<?php
+	include("../koneksi/connect.php");
+	session_start();
+	if(isset($_SESSION['user'])){
+		session_destroy();
+		echo "<script> alert('Sampai Jumpa Lagi!');
+		 	  window.location.href='../login.php';	</script>";
+	}
+	
+?>
