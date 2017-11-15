a25973e4ccc26392d487461aa166ec522ed35834
Nurul Fatikah Muchlis <14523293@students.uii.ac.id>
2017-11-16 02:20:33 +0700

    tampilan crud admin

--- Frontend Preggie/js/js.js
+++ Frontend Preggie/js/js.js
@@ -11,4 +11,63 @@ function change()
     
 }
 
+function konfirmasi_reset()
+{
+	var konfirmasi = confirm("Apakah Anda yakin ingin mereset data ?")
+	
+	if (konfirmasi == true)
+	{
+		return true;
+	}	
+	else
+	{
+		return false;
+	}
+}
+
+function konfirmasi_hapus()
+{
+	var konfirmasi = confirm("Apakah Anda yakin ingin menghapus data ?")
+	
+	if (konfirmasi == true)
+	{
+		return true;
+	}
+
+	else
+	{
+		return false;
+	}
+}
+
+function konfirmasi_tambah()
+{
+	var konfirmasi = confirm("Apakah Anda yakin ingin menambah data ini?")
+
+	if (konfirmasi == true)
+	{
+		return true;
+	}
+
+	else
+	{
+		return false;
+	}
+}
+
+function konfirmasi_ubah()
+{
+	var konfirmasi = confirm("Apakah Anda yakin ingin mengubah data ini?")
+
+	if (konfirmasi == true)
+	{
+		return true;
+	}
+
+	else
+	{
+		return false;
+	}
+}
+
 
